f8d71785eff558a786ca9a1005f5ea413c9e79c6
yanghryu <yanghryu@signzzang.com>
2020-10-11 16:32:08 +0900

    create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+const customers = [
+  {
+    'id':1,
+    'image':'./image/1.jpg',
+    'name':'홍길동',
+    'birthday':'20010320',
+    'gender':'남자',
+    'job':'programer'
+  }
+,
+{
+  'id':2,
+  'image':'./image/2.jpg',
+  'name':'이순신',
+  'birthday':'20010320',
+  'gender':'남자',
+  'job':'장군'
+},
+{
+  'id':3,
+  'image':'./image/1.jpg',
+  'name':'유관순',
+  'birthday':'20010320',
+  'gender':'여자',
+  'job':'운동가'
+}
+]; 
 
-function App() {
+function App()  {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management stystem!</h2>
+    <div>
+    {
+      customers.map(c => {
+        return(
+          <Customer 
+            key = {c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />          
+
+        )
+      })
+    }
     </div>
+  
   );
 }
 
